f469d33a7460d73b3d4cd359dfbfb6416d6730f6
nekozawa666 <nekozawa@inbox.ru>
2019-03-22 19:36:42 +0500

    time

--- app/src/main/java/e/nekozawa/a1l1/MainActivity.java
+++ app/src/main/java/e/nekozawa/a1l1/MainActivity.java
@@ -2,6 +2,9 @@ package e.nekozawa.a1l1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int currentTime= Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        TextView textView=findViewById(R.id.greetings);
+        textView.setText(getString(R.string.hello)+" "+currentTime);
     }
-ggg
 }
